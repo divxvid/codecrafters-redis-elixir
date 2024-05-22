20c026ceb625c1b5e233e43d0a79f5e2730ee110
divxvid <divyansh.shrivastava@students.iiit.ac.in>
2024-05-22 14:35:03 +0530

    [WIP] Implement PING

--- lib/server.ex
+++ lib/server.ex
@@ -21,6 +21,15 @@ defmodule Server do
     # # Since the tester restarts your program quite often, setting SO_REUSEADDR
     # # ensures that we don't run into 'Address already in use' errors
     {:ok, socket} = :gen_tcp.listen(6379, [:binary, active: false, reuseaddr: true])
-    {:ok, _client} = :gen_tcp.accept(socket)
+    {:ok, client} = :gen_tcp.accept(socket)
+
+    {:ok, data} = :gen_tcp.recv(client, 0)
+
+    resp =
+      case data do
+        "PING" -> "+PONG\r\n"
+      end
+
+    :gen_tcp.send(client, resp)
   end
 end
